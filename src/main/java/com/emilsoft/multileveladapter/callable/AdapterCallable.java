c904cbfe2499cdf2cfc46bb1c4f9b94c65bfc53a
e.osterhed <e.osterhed@campus.unimib.it>
2020-06-10 12:06:25 +0200

    Remove old files

--- src/main/java/com/emilsoft/multileveladapter/callable/AdapterCallable.java
+++
@@ -1,9 +0,0 @@
-package com.emilsoft.multileveladapter.callable;
-
-import java.util.concurrent.Callable;
-
-public interface AdapterCallable<V> extends Callable<V> {
-
-    public void onComplete(V result);
-
-}
